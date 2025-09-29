399e304db66b6ac022442b1c3a7654a423de1560
leminhohoho <leminhnguyenai@gmail.com>
2025-09-29 16:17:11 +0000

    Add app

---
+++ scraper/pkg/app/app.go
@@ -0,0 +1,59 @@
+package app
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/leminhohoho/movie-lens/scraper/pkg/logger"
+	"github.com/leminhohoho/movie-lens/scraper/pkg/scraper"
+)
+
+type App struct {
+	Logger  *slog.Logger
+	Scraper *scraper.Scraper
+
+	ErrChan chan error
+}
+
+func NewApp() (*App, error) {
+	var err error
+
+	app := &App{
+		ErrChan: make(chan error),
+	}
+
+	app.Logger, err = logger.NewLogger()
+	if err != nil {
+		return nil, err
+	}
+
+	app.Scraper, err = scraper.NewScraper(app.Logger, app.ErrChan)
+	if err != nil {
+		return nil, err
+	}
+
+	return app, nil
+}
+
+func (a *App) Run() error {
+	a.Logger.Debug(
+		"scraper info",
+		"db_path", os.Getenv("DB_PATH"),
+		"proxy_url", os.Getenv("PROXY_URL"),
+		"headless", os.Getenv("HEADLESS") == "TRUE",
+		"browser_addr", os.Getenv("BROWSER_ADDR"),
+		"user_data_dir", os.Getenv("USER_DATA_DIR"),
+		"debug", os.Getenv("DEBUG") == "TRUE",
+		"silent", os.Getenv("SILENT") == "TRUE",
+	)
+
+	if err := a.Scraper.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *App) Close() {
+	close(a.ErrChan)
+}
