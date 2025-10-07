8eae6d7cea00570909d1754363da2ab0bbaf4cbf
leminhohoho <leminhnguyenai@gmail.com>
2025-10-07 12:39:19 +0000

    Add logger to NavigateTillTrigger

--- scraper/pkg/utils/actions.go
+++ scraper/pkg/utils/actions.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"context"
 	"fmt"
+	"log/slog"
 	"math/rand/v2"
 	"strings"
 	"time"
@@ -49,18 +50,21 @@ func ToGoqueryDoc(sel string, doc **goquery.Document) chromedp.ActionFunc {
 // NavigateTillTrigger run chromedp.Navigate() and a list of chromedp.Action simultaneously (the actions are ran sequentially).
 // If the actions are all finished, the function will close.
 // If no action is provided, it will run chromedp.Navigate concurrently
-func NavigateTillTrigger(url string, actions ...chromedp.Action) chromedp.ActionFunc {
+func NavigateTillTrigger(url string, logger *slog.Logger, actions ...chromedp.Action) chromedp.ActionFunc {
 	return func(ctx context.Context) error {
 		navErrChan := make(chan error, 1)
 		actErrChan := make(chan error, 1)
 
+		logger.Debug("start navigation", "url", url, "tags", []string{"helper"})
 		go func() { navErrChan <- chromedp.Navigate(url).Do(ctx) }()
 		go func() {
-			for _, action := range actions {
+			for i, action := range actions {
 				err := action.Do(ctx)
 				if err != nil {
 					actErrChan <- err
 				}
+
+				logger.Debug("action finished", "order", i+1, "tags", []string{"helper"})
 			}
 
 			actErrChan <- nil
